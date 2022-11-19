73502c58e238118d3ee8855160b35a93f24abc59
Misaki1505 <sawako1505@gmail.com>
2022-11-18 19:09:20 -0500

    18/11/2022 changes

--- app/src/main/java/com/example/kotlinpractices/Functions2.kt
+++ app/src/main/java/com/example/kotlinpractices/Functions2.kt
@@ -5,9 +5,23 @@ fun main(args: Array<String>) {
     val area = calculateCircleArea(r)
     println("A circle with the radius $r has an area of $area")
 
+    val people = listOf("Ana", "Pao", "Wendy")
+    for(person in people) {
+        val message = personalisedGreeting(person)
+        println(message)
+    }
 }
 
 fun calculateCircleArea(radius: Int): Double {
     val pi = 3.1415
     return pi * radius * radius
+}
+
+fun personalisedGreeting(person: String): String {
+    val greeting = when(person[0]) {
+        'A' -> "Are you ok $person"
+        'W' -> "What's up $person"
+        else -> "Hi $person"
+    }
+    return greeting
 }
