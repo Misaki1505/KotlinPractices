a545d3a81e364015f5f86961ae8dbb6770b73985
Misaki1505 <sawako1505@gmail.com>
2022-11-20 20:51:42 -0500

    20/11/2022 changes

--- app/src/main/java/com/example/kotlinpractices/MoreFunctions.kt
+++ app/src/main/java/com/example/kotlinpractices/MoreFunctions.kt
@@ -2,12 +2,18 @@ package com.example.kotlinpractices
 
 fun main(args: Array<String>) {
     for(i in 1..3) {
-        val p1 = 2
-        val p2 = 1
         println(addition(i*5, i*9))
+    }
+
+    val products = hashMapOf(Pair("shoes", 29.99), Pair("socks", 5.99), Pair("Jeans", 3.99))
+    for(item in products.keys) {
+        val finalPrice = addTax(products[item])
+        println("$item cost $finalPrice")
     }
 }
 
 fun addition(p1: Int, p2: Int): Int {
     return p1 + p2
 }
+
+fun addTax(price: Double?) = price?.times( 1.2)
