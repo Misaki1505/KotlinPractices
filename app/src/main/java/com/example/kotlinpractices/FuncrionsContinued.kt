4c184e5a8f7c46538c6b89577bc7295e8e3eeeb4
Misaki1505 <sawako1505@gmail.com>
2022-11-22 18:50:53 -0500

    22/11/2022 changes

--- app/src/main/java/com/example/kotlinpractices/FuncrionsContinued.kt
+++ app/src/main/java/com/example/kotlinpractices/FuncrionsContinued.kt
@@ -3,8 +3,21 @@ package com.example.kotlinpractices
 fun main(args: Array<String>) {
     println(multiply(5))
     println(multiply(7,9))
+
+    feedAnimal("cat")
+    feedAnimal(setOf("dog", "bear", "shark"))
 }
 
 fun multiply(number: Int) = number * 2
 
 fun multiply(number: Int, multiplier: Int) = number * multiplier
+
+fun feedAnimal(animal: String) {
+    println("Feeding the $animal")
+}
+
+fun feedAnimal(animals: Collection<String>) {
+    for(animal in animals) {
+        feedAnimal(animal)
+    }
+}
