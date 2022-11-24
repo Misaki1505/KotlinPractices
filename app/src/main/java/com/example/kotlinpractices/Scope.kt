0ce448a04591e9929f7e0e63ab48f2ac3c437a9f
Misaki1505 <sawako1505@gmail.com>
2022-11-23 19:47:55 -0500

    23/11/2022 changes

---
+++ app/src/main/java/com/example/kotlinpractices/Scope.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinpractices
+
+fun main(args: Array<String>) {
+    val year = 2015
+    printYear()
+    println("The year inside main is $year")
+
+}
+
+fun printYear() {
+    val year = 1985
+    println("The year inside de function is $year")
+}
