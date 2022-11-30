8187701fa6ed5f8aec08694e8e911fa117873139
Misaki1505 <sawako1505@gmail.com>
2022-11-29 19:43:32 -0500

    29/11/2022 changes

--- app/src/main/java/com/example/kotlinpractices/HorderFunc.kt
+++ app/src/main/java/com/example/kotlinpractices/HorderFunc.kt
@@ -4,4 +4,13 @@ fun main(args: Array<String>) {
     val client = listOf("Alice", "Bob", "Sandra","Susana")
     client.filter { it.length < 5 }
     .forEach { println("Hello $it") }
+
+    val number = setOf(3, 546, 88, 102, 91, 15, 1542)
+    println(number.filter{ it in 10..99})
+
+    val clients = listOf("Tom", "Laura", "León", "Clara", "Robin")
+    println(clients.sortedBy { it[it.length-1] })
+
+    val ints = listOf(295, 895, 784, 126, 542, 982)
+    println(ints.maxBy { it.toString()[1].toInt() })
 }
