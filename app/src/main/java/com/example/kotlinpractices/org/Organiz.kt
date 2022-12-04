2818f5236d1f7a25f271f460c93a4f73bd6d752a
Misaki1505 <sawako1505@gmail.com>
2022-12-03 20:48:58 -0500

    03/12/2022 changes

--- app/src/main/java/com/example/kotlinpractices/org/Organiz.kt
+++ app/src/main/java/com/example/kotlinpractices/org/Organiz.kt
@@ -3,3 +3,7 @@ package com.example.kotlinpractices.org
 fun hi() {
     println("Hello how are you?")
 }
+
+fun sayHi(clientname: String) {
+    println("Hello $clientname")
+}
--- app/src/main/java/com/example/kotlinpractices/run/run.kt
+++ app/src/main/java/com/example/kotlinpractices/run/run.kt
@@ -1,7 +1,9 @@
 package com.example.kotlinpractices.run
 
 import com.example.kotlinpractices.org.hi
+import com.example.kotlinpractices.org.sayHi
 
 fun main(args: Array<String>) {
     hi()
+    sayHi("Leonard")
 }
